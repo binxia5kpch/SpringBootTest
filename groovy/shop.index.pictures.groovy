4aeab455685fd9f94eb28f88955fef57f2e317db
binxia5kpch <309995943@qq.com>
2017-11-20 17:44:50 +0800

    上传项目SpringBootTest

--- groovy/shop.index.pictures.groovy
+++ groovy/shop.index.pictures.groovy
@@ -1,9 +1,11 @@
+import com.alibaba.fastjson.JSON
+
 String a="""
 
 carousel_list: [
 {
 id: "5954cd0d5b73294d45cbd71f",
-field_mark: 3,
+field_mark: "3",
 event_mark: 3,
 event_memo: "5954cbd35b73294d45cbd71d",
 img: "/system/carousel_applies/avatars/5954/cc9d/5b73/294d/497e/121c/original/QQ%E5%9B%BE%E7%89%8720170628132417.jpg?1498729629",
@@ -30,4 +32,19 @@ url: null
 
 """
 
-out <<a
+//out <<a
+
+def backMap=[:]
+def carousel_list=[];
+
+def categoryMap=[:]
+
+categoryMap.put("id","5922bed95b73291ab08800c2")
+categoryMap.put("field_mark","6")
+categoryMap.put("event_mark","0")
+categoryMap.put("img","/system/carousel_applies/avatars/5922/bed9/5b73/291a/b088/00c1/original/0522%E5%A4%A9%E6%9C%9D%E4%B8%8A%E5%93%81%E8%B4%B5%E4%BA%BA%E9%85%92%E8%BD%AE%E6%92%AD.jpg?1495449305")
+carousel_list.add(categoryMap)
+backMap.put("carousel_list",carousel_list)
+
+
+out << JSON.toJSONString(backMap)
