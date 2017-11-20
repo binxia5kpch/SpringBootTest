5f02cd392735503745c5c958fd5d0ea375d17af2
binxia5kpch <309995943@qq.com>
2017-11-20 16:22:41 +0800

    上传项目SpringBootTest

---
+++ groovy/shop.index.list.groovy
@@ -0,0 +1,24 @@
+import com.alibaba.fastjson.JSON
+import com.zhongsou.dao.mysql.GoodsMysql
+import com.zhongsou.util.BeanUtils
+
+GoodsMysql goodsMysql = BeanUtils.getBean("goodsMysql")
+
+def productList=[]
+
+
+goodsMysql.shopMysql.eachRow("select * from shop_goods",{
+    productList<<[
+            id:it.id,
+            qrcode:it.qrcode,
+            title:it.title,
+            price:it.price,
+            avatar_url:it.avatar_url,
+            thumb_url:it.thumb_url,
+            marketPrice:it.market_price,
+            mobile_category_id:it.mobile_category_id,
+            category_name:it.category_name
+    ]
+})
+
+out << JSON.toJSONString(productList)
