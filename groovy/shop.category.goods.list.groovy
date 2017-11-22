2d54302999b70b712d3ff09de954634acaaa1d02
binxia5kpch <309995943@qq.com>
2017-11-22 10:24:24 +0800

    上传项目SpringBootTest

--- groovy/shop.category.goods.list.groovy
+++ groovy/shop.category.goods.list.groovy
@@ -2,16 +2,25 @@ import com.alibaba.fastjson.JSON
 import com.zhongsou.dao.mysql.GoodsMysql
 import com.zhongsou.util.BeanUtils
 
+import javax.servlet.http.HttpServletRequest
 
 /**
- * 首页列表
+ * 通过分类查询商品列表
  */
 GoodsMysql goodsMysql = BeanUtils.getBean("goodsMysql")
+
+
+
+HttpServletRequest re = request as HttpServletRequest
+String cateId = re.getParameter("cateId");
+
+
+
 
 def productList=[]
 
 
-goodsMysql.shopMysql.eachRow("select * from shop_goods where is_index=1",{
+goodsMysql.shopMysql.eachRow("select * from shop_goods where mobile_category_id=?",[cateId],{
     productList<<[
             id:it.id,
             qrcode:it.qrcode,
